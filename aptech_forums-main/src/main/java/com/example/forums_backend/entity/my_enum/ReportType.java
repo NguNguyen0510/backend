963e5813674b7ecd91a12ff1fb858ae968b5181d
NguNguyen0510 <93904442+NguNguyen0510@users.noreply.github.com>
2023-11-22 12:29:49 +0700

    1
    sửa và build

--- aptech_forums-main/src/main/java/com/example/forums_backend/entity/my_enum/ReportType.java
+++ aptech_forums-main/src/main/java/com/example/forums_backend/entity/my_enum/ReportType.java
@@ -4,7 +4,13 @@ package com.example.forums_backend.entity.my_enum;
 public enum ReportType {
     SPAM,
     INSULTS,
-    False_content,
+    ILLEGAL_TRADE,
+    ADULT_CONTENT,
+    DISCRIMINATION,
+    CHEAT,
+    CHILD_ABUSE,
+    FALSE_CONTENT,
+    OTHER,
     // Thêm các loại report khác nếu cần
 }
 
